c0452ec985ab7073482441e0a33f67762f262dc4
Muhammad Riza <Muhammad Riza@LAPTOP-065M04I0>
2022-01-13 13:00:32 +0530

    hello

---
+++ dummy/src/dummy/testing.java
@@ -0,0 +1,22 @@
+package dummy;
+import java.util.Scanner;
+public class testing {
+	public static void main(String[] args){
+	int a;
+	int b;
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number");
+	a=sc.nextInt();
+	
+	Scanner dc=new Scanner(System.in);
+	System.out.println("enter the number");
+	b=dc.nextInt();
+	
+	int c=a+b;
+	System.out.println(c);
+	
+	}
+	
+
+}
